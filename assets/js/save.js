0123884fd2dd8bf126b27431298f493a1b4a2704
QuirkyBirdy <48087728+SmNkBirdy@users.noreply.github.com>
2021-05-17 15:58:01 +0500

    Add files via upload

--- assets/js/save.js
+++ assets/js/save.js
@@ -5,20 +5,26 @@ chrome.storage.local.get("allQuests", value => {
     data = ""
     console.log(value['allQuests']);
     for (const quest of value['allQuests']) {
-        data += "@" + quest[1] + "\n";
+        data += "insert into table1 (id, question, answer1, answer2, answer3, answer4, answer5, explanation, correctanswer) values ("
+        data += '"' + quest[0] + '", "' + quest[1] + '"';
         i = 2;
+        correctAnsw = "";
         while (quest[i].startsWith("+") || quest[i].startsWith("-")) {
             if(quest[i].startsWith("+")){
-                data += "!" + quest[i].substring(1) + "\n";
+                correctAnsw += quest[i] + " ";
+                data += ', "' + quest[i].substring(1);
             }else{
-                data += "~" +quest[i].substring(1) + "\n";
+                data += ', "' + quest[i].substring(1);
             }
             i++;
         }
-        data += "[" + quest[i] + "\n]\n#\n"
+        for (let l = i; l < 7; l++) {
+            data += ', ""';
+        }
+        data += ', "' + quest[i] + '");';
     }
 
-    filename = "данные.txt"
+    filename = "данные.sql"
     type = "text"
     var file = new Blob([data], {type: type});
     if (window.navigator.msSaveOrOpenBlob) // IE10+
